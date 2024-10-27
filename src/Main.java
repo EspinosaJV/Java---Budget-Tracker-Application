5097169d12c7f57eb9b151b4f17de5c088fcf33c
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2024-10-28 00:16:46 +0800

    + Completion of Budget Tracker Application

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,17 @@ public class Main {
 
         // single variables
         String monthBudget = "";
+        String expenseCategoryInput = "";
         double monthlyBudgetValue = 0;
+        double expenseValueInput = 0;
+        double expenseValueTotal = 0;
+        boolean isBudgetExceeded = false;
+
 
         // array variables
         List<String> months = Arrays.asList("january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december");
         List<String> categoryExpenses = new ArrayList<>();
+        List<Double> valueExpenses = new ArrayList<>();
 
         //Prompt user for what month to budget
         while (true) {
@@ -38,7 +44,41 @@ public class Main {
 
         //Prompt user to be able to enter the expense category
         while (true) {
-            System.out.println("");
+            System.out.println("We will now be calculating the expenses, type exit to finish.");
+            System.out.print("Enter the category of the expense: ");
+            expenseCategoryInput = scanner.next();
+
+            if (expenseCategoryInput.equals("exit"))
+            {
+                break;
+            }
+
+            categoryExpenses.add(expenseCategoryInput);
+            System.out.print("Enter your total expenses for this specific category: ");
+            expenseValueInput = scanner.nextDouble();
+            valueExpenses.add(expenseValueInput);
+            expenseValueTotal += expenseValueInput;
+            expenseValueInput = 0;
+
+            if ((monthlyBudgetValue - expenseValueTotal) < 0)
+            {
+                System.out.println("You have exceeded your monthly budget by " + (expenseValueTotal - monthlyBudgetValue));
+                isBudgetExceeded = true;
+                break;
+            }
+        }
+
+        if (isBudgetExceeded)
+            System.out.println("Your expenses has exceeded your monthly budget, please reallocate!");
+        else
+        {
+            for (int i = 0; i < categoryExpenses.size(); i++)
+            {
+                System.out.println("For category " + categoryExpenses.get(i) + " you had an expense of " + valueExpenses.get(i));
+            }
+
+            System.out.println("Your total expense for the month was: " + expenseValueTotal);
+            System.out.println("Your remaining budget for the month is: " + (monthlyBudgetValue - expenseValueTotal));
         }
 
     }
